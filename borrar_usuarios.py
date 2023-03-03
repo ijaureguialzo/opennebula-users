f744b45f10e5eafcbbf462610c4a86acfc792a8a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2023-03-04 00:18:14 +0100

    Mensajes mejorados

--- borrar_usuarios.py
+++ borrar_usuarios.py
@@ -3,14 +3,24 @@
 import os
 import click
 
-print("--- Datos del usuario ---")
+print()
+print("-------------------------------------")
+print("--- Borrar usuarios de OpenNebula ---")
+print("-------------------------------------")
+print()
+
+print("--- Datos del usuario ---------------")
 nombre = click.prompt("Nombre de usuario", default="ciber")
 separador = click.prompt("Separador", default="-")
 inicial = click.prompt("Número de usuario inicial", default=1)
 final = click.prompt("Número de usuario final", default=15)
 
+print()
+
 for i in range(inicial, final + 1):
     usuario = nombre + separador + "{0:0>2}".format(i)
+
+    print(f"Borrando el usuario {usuario} y sus recursos asociados...")
 
     os.system("onevdc delete " + usuario)
     os.system("oneuser delete " + usuario)
--- crear_usuarios.py
+++ crear_usuarios.py
@@ -3,7 +3,13 @@
 import os
 import click
 
-print("--- Datos del usuario ---")
+print()
+print("------------------------------------")
+print("--- Crear usuarios de OpenNebula ---")
+print("------------------------------------")
+print()
+
+print("--- Datos del usuario --------------")
 nombre = click.prompt("Nombre de usuario", default="ciber")
 separador = click.prompt("Separador", default="-")
 inicial = click.prompt("Número de usuario inicial", default=1)
@@ -11,21 +17,26 @@ final = click.prompt("Número de usuario final", default=15)
 contrasenya = click.prompt("Contraseña", hide_input=True, confirmation_prompt=True)
 grupo = click.prompt("Grupo de usuarios", default="ciberseguridad")
 
-print("--- Cuota de recursos ---")
+print("--- Cuota de recursos --------------")
 cpus = click.prompt("CPUs", type=float, default=4)
 ram = click.prompt("RAM (GiB)", type=float, default=12)
 disco = click.prompt("Disco (GiB)", type=float, default=64)
 
-print("--- Red del usuario ---")
+print("--- Red del usuario ----------------")
 subred = click.prompt("Subred", default="172.20.227.0")
 mascara = click.prompt("Máscara", default="255.255.0.0")
 gateway = click.prompt("Puerta de enlace", default="172.20.1.2")
 dns = click.prompt("Servidores de nombres", default="192.168.10.1 1.1.1.1")
 
+print()
+
+print(f"Creando el grupo {grupo}...")
 os.system("onegroup create " + grupo)
 
 for i in range(inicial, final + 1):
     usuario = nombre + separador + "{0:0>2}".format(i)
+
+    print(f"Creando el usuario {usuario} y sus recursos asociados...")
 
     os.system("onevdc create " + usuario)
     os.system("onegroup create " + usuario)
